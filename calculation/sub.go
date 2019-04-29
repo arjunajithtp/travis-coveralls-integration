1b920225f07fc35e695740f2d6a2cf64ac3d3e9b
arjun ajith <arjun@MSSHI7.asiapacific.hpqcorp.net>
2019-04-29 14:59:13 +0530

    corrected lint errors

--- calculation/sub.go
+++ calculation/sub.go
@@ -1,5 +1,6 @@
 package calculation
 
+// Sub is for finding sub
 func Sub(a, b int) int {
 	return a - b
 }
--- calculation/sum.go
+++ calculation/sum.go
@@ -2,7 +2,8 @@ package calculation
 
 import "fmt"
 
+// Sum is for finding sum
 func Sum(a, b int) int {
-	fmt.Printf("ree: ", a)
+	fmt.Printf("values: %v %v", a, b)
 	return a + b
 }
