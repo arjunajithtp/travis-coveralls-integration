cc99c7795a0b716824d940b584b0b80e5436a404
arjun ajith <arjun@MSSHI7.asiapacific.hpqcorp.net>
2019-04-29 16:40:54 +0530

    corrected the test case

--- calculation/sub_test.go
+++ calculation/sub_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSub(t *testing.T) {
 	total := Sub(5, 5)
-	if total != 2 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	if total != 0 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 0)
 	}
 }
